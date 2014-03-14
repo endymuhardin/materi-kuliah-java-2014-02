5eed9bf80515a0cd304ec290bb6eb80025023f5e
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-14 10:45:58 +0700

    implement hitung bulan 30 dan 31

--- sample-code/belajar-junit/src/main/java/com/muhardin/endy/belajar/junit/DateUtil.java
+++ sample-code/belajar-junit/src/main/java/com/muhardin/endy/belajar/junit/DateUtil.java
@@ -2,6 +2,19 @@ package com.muhardin.endy.belajar.junit;
 
 public class DateUtil {
     public Integer hitungHari(Integer tahun, Integer bulan){
-        return 0;
+        switch(bulan){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12: return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11: return 30;
+            default: throw new IllegalArgumentException("Bulan harus di antara 1 - 12");
+        }
     }
 }
--- sample-code/belajar-junit/src/test/java/com/muhardin/endy/belajar/junit/DateUtilTest.java
+++ sample-code/belajar-junit/src/test/java/com/muhardin/endy/belajar/junit/DateUtilTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 public class DateUtilTest {
     @Test
-    public class testHitungHari(){
+    public void testHitungHari31(){
         DateUtil d = new DateUtil();
         
         // hasil yang diharapkan
@@ -15,6 +15,20 @@ public class DateUtilTest {
         Integer hasilAktual = d.hitungHari(2014,1);
         
         // suruh JUnit membandingkan
-        assertEquals(hasilAktual, hasilSeharusnya);
+        assertEquals(hasilSeharusnya, hasilAktual);
+    }
+    
+    @Test
+    public void testHitungHari30(){
+        DateUtil d = new DateUtil();
+        
+        // hasil yang diharapkan
+        Integer hasilSeharusnya = 30;
+        
+        // hasil yang keluar
+        Integer hasilAktual = d.hitungHari(2014,4);
+        
+        // suruh JUnit membandingkan
+        assertEquals(hasilSeharusnya, hasilAktual);
     }
 }
