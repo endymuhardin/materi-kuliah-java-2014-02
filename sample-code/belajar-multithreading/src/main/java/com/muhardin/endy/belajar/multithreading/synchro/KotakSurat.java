c6229920457b6b5913a6399f360d50b57f0d02d1
Endy Muhardin <endy.muhardin@gmail.com>
2014-05-23 10:51:59 +0700

    contoh sinkronisasi antar thread

--- sample-code/belajar-multithreading/src/main/java/com/muhardin/endy/belajar/multithreading/synchro/KotakSurat.java
+++ sample-code/belajar-multithreading/src/main/java/com/muhardin/endy/belajar/multithreading/synchro/KotakSurat.java
@@ -3,15 +3,38 @@ package com.muhardin.endy.belajar.multithreading.synchro;
 public class KotakSurat {
 	
 	private Integer jumlahSurat = 0;
+	private Boolean kosong = true;
 
-	public void taruhSurat() {
-		System.out.println("Tukang pos menaruh surat");
-		jumlahSurat++;
+	public synchronized void taruhSurat() {
+		try {
+			while(!kosong){
+				System.out.println("Masih ada isinya, tunggu kosong dulu");
+				wait();
+			}
+
+			System.out.println("Tukang pos menaruh surat");
+			jumlahSurat++;
+			kosong = false;
+			notifyAll();
+		} catch (Exception err){
+			err.printStackTrace();
+		} 
 	}
 
-	public void ambilSurat(){
-		System.out.println("Tuan rumah mengambil surat");
-		jumlahSurat--;
+	public synchronized void ambilSurat(){
+		try {
+			while(kosong){
+				System.out.println("Belum ada isinya, tunggu diisi dulu");
+				wait();
+			}
+
+			System.out.println("Tuan rumah mengambil surat");
+			jumlahSurat--;
+			kosong = true;
+			notifyAll();
+		} catch (Exception err){
+			err.printStackTrace();
+		} 
 	}
 
 	public void tampilkanIsi(){
